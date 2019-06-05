3152a0256e6736d12f0fc54cddf1f8d56a752bf3
Lana Monson <44383292+LanaMonson@users.noreply.github.com>
2019-06-04 21:13:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
 const monk = require('monk')
-const url = 'mongodb://lanamonson:September20$@cluster0-shard-00-00-9jv93.mongodb.net:27017,cluster0-shard-00-01-9jv93.mongodb.net:27017,cluster0-shard-00-02-9jv93.mongodb.net:27017/utah?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+const url = 'mongodb://111:111@cluster0-shard-00-00-9jv93.mongodb.net:27017,cluster0-shard-00-01-9jv93.mongodb.net:27017,cluster0-shard-00-02-9jv93.mongodb.net:27017/utah?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
 const db = monk(url)
     db.then(() => {
             console.log('connected')
     })
 const images = db.get('images')
 
+app.use(cors())
 app.use(bodyParser.json())
 
 //GET
